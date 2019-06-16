c8d7bb192fdc49af2c6d2a44fe9c5355644ded78
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-06-16 21:51:50 +0400

    Tic-Tac-Toe

---
+++ Tic-Tac-Toe_WPF/Tic-Tac-Toe_WPF/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tic_Tac_Toe_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Color()
+        {
+
+            Container.Children.Cast<Button>().ToList().ForEach(button =>
+            {
+                button.Content = string.Empty;
+            });
+        }
+
+        private void B0_0_Click(object sender, RoutedEventArgs e)
+        {
+            if (e.Handled)
+            {
+
+            }
+        }
+    }
+}
